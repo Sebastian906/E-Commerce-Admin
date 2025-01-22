c0056edacfed96948e990f40673e901d8a92d346
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2025-01-22 18:12:37 -0500

    Configuración de la interfaz gráfica para el formulario de inicio de sesión

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const Login = () => {
     return (
-        <div>
-
+        <div className='min-h-screen flex items-center justify-center w-full'>
+            <div className='bg-slate-50 shadow-md rounded-lg px-8 py-6 max-w-md'>
+                <h1 className='text-2xl font-bold mb-4'>Panel de Administrador</h1>
+                <form>
+                    <div className='mb-3 min-w-72'>
+                        <p className='text-sm font-medium text-gray-700 mb-2'>Dirección de Correo</p>
+                        <input className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' type="email" placeholder='your@email.com' required />
+                    </div>
+                    <div className='mb-3 min-w-72'>
+                        <p className='text-sm font-medium text-gray-700 mb-2'>Contraseña</p>
+                        <input className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' type="password" placeholder='Ingrese su contraseña' required />
+                    </div>
+                    <button className='mt-2 w-full py-2 px-4 rounded-md text-slate-50 bg-slate-900' type="submit"> Iniciar Sesión </button>
+                </form>
+            </div>
         </div>
     )
 }
