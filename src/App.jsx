be29b168c486717736e77c9157547609c30f68ac
unknown <sebastian.1701811396@ucaldas.edu.co>
2025-01-29 11:38:15 -0500

    Configuración para recargar la interfaz grafica con usuario logeado dentro del panel de administrador

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import { Routes, Route } from 'react-router-dom'
@@ -7,17 +7,23 @@ import List from './pages/List'
 import Orders from './pages/Orders'
 import { useState } from 'react'
 import Login from './components/Login'
+import { ToastContainer } from 'react-toastify';
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL
 
 const App = () => {
 
-  const [token, setToken] = useState('');
+  const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):'');
+
+  useEffect(()=>{
+    localStorage.setItem('token',token)
+  },[token])
 
   return (
     <div className='bg-slate-100 min-h-screen'>
+      <ToastContainer/>
       { token === "" 
-        ? <Login/> 
+        ? <Login setToken={setToken}/> 
         : <>
           <Navbar/>
           <hr />
